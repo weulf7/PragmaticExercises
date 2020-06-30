02d544c4c978944543b23622e5ff4cbd910b0a0f
Antal Matyas <antalmatyas9@gmail.com>
2020-06-30 11:45:14 +0300

    resolved exercises to number22

---
+++ src/main/java/Exercise22/Main.java
@@ -0,0 +1,35 @@
+package Exercise22;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+        comparingNumbers();
+
+
+
+    }
+    public static void comparingNumbers(){
+        System.out.println("Enter the first number:");
+        Scanner scanner = new Scanner(System.in);
+        int first=scanner.nextInt();
+
+        System.out.println("Enter the second number:");
+        int second=scanner.nextInt();
+
+        System.out.println("Enter the third number:");
+        int third=scanner.nextInt();
+
+       if (first>second && first>third){
+           System.out.println(first+" is the largest number.");
+       }
+       else if (second>first && second>third){
+           System.out.println(second+" is the largest number.");
+       }
+       else{
+           System.out.println(third +" is the largest number.");
+       }
+
+
+    }
+}
